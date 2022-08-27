a7a78f9dba598c623c19eacb0b8250d8a0e68603
Nikola Dimic <dimic.nikola@gmail.com>
2022-08-27 14:44:31 +0200

    Added mock data flag

--- project/client/src/index.js
+++ project/client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const { worker } = require("./mocks/browser");
-worker.start();
+console.log(process.env);
+if (process.env.REACT_APP_MOCK_DATA) {
+  console.log("Mocking responses from the server");
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
